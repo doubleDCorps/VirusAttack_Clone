0fd20960d443bbf45e410d8376b6572046d4be1e
Dygwah98 <36772959+Dygwah98@users.noreply.github.com>
2019-03-28 18:50:34 +0100

    Revert "Merge branch 'EverythingIsLevel'"
    This reverts commit d45a51053169f4f24aa58d4c0305c708d80742bc, reversing
    changes made to 4b888e0376f86742e0f6b3e3cd36165304433bbb.

--- Entity.h
+++ Entity.h
@@ -38,13 +38,6 @@ enum KEYS : int {still=0, UP=1, LEFT=2, DOWN=3, RIGHT=4, SPACE=5};
      && (in_range(y1a, min(y2a, y2b), max(y2a, y2b)) || in_range(y1b, min(y2a, y2b), max(y2a, y2b)) ||
          in_range(y2a, min(y1a, y1b), max(y1a, y1b)) || in_range(y2b, min(y1a, y1b), max(y1a, y1b)) );
 }
-
- pair<float, float> normale1(pair<float, float> p, float v) { return {p.second+v, p.first}; }
- pair<float, float> normale2(pair<float, float> p, float v) { return {p.second-v, p.first}; }
- pair<float, float> normale3(pair<float, float> p, float v) { return {p.second, p.first+v}; }
- pair<float, float> normale4(pair<float, float> p, float v) { return {p.second, p.first-v}; }
-
-
 /*
     Tre strutture dati elementari che definiscono i seguenti dati:
         PointData:  coordinata x, coordinata y
--- Level.h
+++ Level.h
@@ -1,100 +1,6 @@
 #ifndef POLY_HPP_
 #define POLY_HPP_
 #include"Entities.h"
-
- initializer_list<perimeter> defPerInit(ALLEGRO_DISPLAY_MODE d)
-{
-    int x{ d.width/2  - 500/2 }, y{ d.height/2 - 500/2 };
-
-    return {
-        {x      , y      },
-        {x + 500, y      },
-        {x + 500, y + 500},
-        {x      , y + 500}
-    };
-}
-
- void spawn(vector<Entity*>& entities)
-{   
-    for(unsigned i{2}; i < entities.size(); i++)
-     if(!entities[i]->isAlive())
-    {
-        entities[i]->setAlive(true);
-        entities[i]->setCord_x(entities[1]->getCord_x());
-        entities[i]->setCord_y(entities[1]->getCord_y());
-    }
-}
-
-  void entities_init(const Player& player, vector<Entity*>& entities)
-{
-    player = {275, 70, 4, al_create_bitmap(30,30)};                                        //PLAYER
-    entities.push_back(new Enemy{float(800)/2-30/2, float(600)/2-30/2, -4.0, -4.0, al_create_bitmap(30,30)});   //BOSS 
-    
-    for(unsigned i=0; i<12; ++i)
-        entities.push_back(new Enemy{float(800)/2-30/2, float(600)/2-30/2, 0, 0, al_create_bitmap(30,30)});     //MINIONS
-
-    const int enemy_velocity{25};
-
-    int cont_minion=2;
-     while(cont_minion < 13)
-    {
-        bool presente{ false };
-        int int_dx{ rand()%(enemy_velocity-12)+2 };
-        int int_dy{ enemy_velocity-int_dx };
-
-        for(unsigned i{1}; i<13; ++i)
-         if(int_dx == entities[i]->getVelocity_x() and int_dy == entities[i]->getVelocity_y() )
-        {
-            presente = true;
-            break;
-        }
-
-         if(!presente)
-        {
-            entities[cont_minion]->setVelocity_x(int_dx);
-            entities[cont_minion]->setVelocity_y(int_dy);
-            cont_minion++;
-        }
-    }
-
-     for(unsigned i{1}; i<13; ++i)
-    {
-         if(i <= 4)
-        {
-            entities[i]->setVelocity_x(float((entities[i]->getVelocity_x())*-1.0)/10);
-            entities[i]->setVelocity_y(float(entities[i]->getVelocity_y())/10);
-        }
-         else if(i >= 5 and i <= 7)
-        {
-            entities[i]->setVelocity_x(float(entities[i]->getVelocity_x())/10);
-            entities[i]->setVelocity_y(float(entities[i]->getVelocity_y())/10);
-        }
-         else if(i >= 8 and i <= 10)
-        {
-            entities[i]->setVelocity_x(float(entities[i]->getVelocity_x())/10);
-            entities[i]->setVelocity_y(float((entities[i]->getVelocity_y())*-1.0)/10); 
-        }
-         else if(i >= 11 and i <= 13)
-        {
-            entities[i]->setVelocity_x(float((entities[i]->getVelocity_x())*-1.0)/10);
-            entities[i]->setVelocity_y(float((entities[i]->getVelocity_y())*-1.0)/10);
-        }
-    }
-
-    al_set_target_bitmap(entities[0]->getBitmap()); //PLAYER_INIT
-    al_clear_to_color(al_map_rgb(0, 255, 0));
-
-    al_set_target_bitmap(entities[1]->getBitmap()); //BOSS_INIT
-    al_clear_to_color(al_map_rgb(255, 0, 0));
-
-     for(unsigned i{1};i<13;i++)
-    {
-        entities[i]->setAlive(false);
-        al_set_target_bitmap(entities[i]->getBitmap());
-        al_clear_to_color(al_map_rgb(0, 0, 0));
-    }
-}
-
 /*
     GameList è una lista di coppie di coordinate circolare e ordinata secondo il seguente criterio:
     per ogni coppia di elementi a1 e a2 consecutivi, la coppia precedente a0 e a1 sarà tale che
@@ -140,12 +46,9 @@
  class Level
 {
     public:
-        Level(unsigned diff, ALLEGRO_DISPLAY_MODE mod): difficulty(diff), defPerimeter(defPerInit(mod))
-        { 
-            /*inizializza numBosses*/
-        }
-        //Level(): border(), trace(), Boss(nullptr), Player(nullptr) {}
-        //Level(const perimeter& b, const Entity* P=nullptr, const Entity* B=nullptr): border(b), trace(), Boss(B), Player(P) {}     
+        Level(): border(), trace(), Boss(nullptr), Player(nullptr) {}
+        Level(const list<PointData>& b, const Entity* P=nullptr, const Entity* B=nullptr)
+            : border(b), trace(), Boss(B), Player(P) {}     
 
         //se si sposta il gameLoop in Level::update, le funzioni qui sotto potrebbero passare a private:
 
@@ -162,15 +65,15 @@
         
         //
 
-        void loop();
+        bool update();
         void clear() { trace.clear(); }
         int getArea() const;
     
     private:
-        unsigned difficulty;
-        static perimeter defPerimeter;
-        unsigned numBosses;
-        //
+        GameList border;
+        GameList trace;
+        const Entity* Boss;
+        const Entity* Player;
 };
 
 #endif
